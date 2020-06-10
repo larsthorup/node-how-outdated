f8bf07544e913baba221eeaf83fc9af5ff590ecb
Lars Thorup <lars@zealake.com>
2020-06-10 20:50:14 +0200

    sort by age descending

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 const { promisify } = require("util");
 const { exec } = require("child_process");
 const executing = promisify(exec);
-const path = require("path");
-const cwd = process.cwd();
 
 async function main() {
+  process.stdout.write(".");
   const { stdout: outdatedStdout } = await new Promise((resolve) => {
     exec("npm outdated --json", (err, stdout) => {
       if (err) {
@@ -16,7 +15,9 @@ async function main() {
     });
   });
   const outdated = JSON.parse(outdatedStdout);
+  const howOutdated = [];
   for (packageName of Object.keys(outdated)) {
+    process.stdout.write(".");
     const outdatedPackage = outdated[packageName];
     const { stdout: viewStdout } = await executing(
       `npm view ${packageName} --json`
@@ -27,6 +28,21 @@ async function main() {
     const agePackage = Math.trunc(
       (Date.now() - Date.parse(currentPackageDate)) / (24 * 60 * 60 * 1000)
     );
+    howOutdated.push({
+      agePackage,
+      packageName,
+      currentPackageVersion,
+      currentPackageDate,
+    });
+  }
+  process.stdout.write("\n");
+  howOutdated.sort((a, b) => (a.agePackage < b.agePackage ? +1 : -1));
+  for ({
+    agePackage,
+    packageName,
+    currentPackageVersion,
+    currentPackageDate,
+  } of howOutdated) {
     console.log(
       agePackage,
       packageName,
